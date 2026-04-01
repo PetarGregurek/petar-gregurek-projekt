f54ae7ea29f7b91cc0a81d07d90cd0567a8175ef
Petar Gregurek <pgregurek@gmail.com>
2026-04-01 17:21:37 +0200

    conf

--- main.cs
+++ main.cs
@@ -7,13 +7,14 @@ class Program
         int num1 = 5;
         int num2 = 10;
         int num3 = 15;
-        int sum = AddNumbers(num1, num2, num3);
+        int num4 = 20;
+        int sum = AddNumbers(num1, num2, num3, num4);
         
-        Console.WriteLine($"The sum of {num1}, {num2}, and {num3} is: {sum}");
+        Console.WriteLine($"The sum of {num1}, {num2}, {num3}, and {num4} is: {sum}");
     }
     
-    static int AddNumbers(int a, int b, int c)
+    static int AddNumbers(int a, int b, int c, int d)
     {
-        return a + b + c;
+        return a + b + c + d;
     }
 }
